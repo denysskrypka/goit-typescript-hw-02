7117e987fa270d93fc9aab38357d77fb3cfbcbf7
denysskrypka <118266939+denysskrypka@users.noreply.github.com>
2024-08-24 18:14:59 +0300

    Update photos-api.ts

--- src/helpers/photos-api.ts
+++ src/helpers/photos-api.ts
@@ -1,14 +1,27 @@
 import axios from "axios";
 
+// Визначаємо тип для параметрів функції
 type GetPhotosParams = {
   query: string;
   currentPage: number;
 };
 
+// Визначаємо тип для результату функції
+interface Photo {
+  id: string;
+  urls: {
+    small: string;
+    regular: string;
+  };
+  description: string;
+}
+
+axios.defaults.baseURL = "https://api.unsplash.com";
+
 export const getPhotos = async (
   query: string,
   currentPage: number
-): Promise<any[]> => {
+): Promise<Photo[]> => {
   const res = await axios.get("/search/photos", {
     params: {
       client_id: "HjCGXgY18QVarShgv2gPDZDfe387Ctdb9cYDEcjcXl4",
